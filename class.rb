222460f55bc03e0c5a74a1c6756e878433133e9e
saich <chsai.btech@gmail.com>
2016-04-30 18:43:04 +0000

    something added

--- class.rb
+++ class.rb
@@ -1,48 +1,50 @@
 #class
 
-class MyStuff
+# class MyStuff
     
-    def initialize()
-        @tangerine = "And now a thousand years between"
-    end
+#     def initialize()
+#         @tangerine = "And now a thousand years between"
+#     end
 
-    attr_reader :tangerine
-    # attr_writer :tangerine
-    # attr_accessor :tangerine
+#     attr_reader :tangerine
+#     # attr_writer :tangerine
+#     # attr_accessor :tangerine
 
-    def apple()
-        puts "I AM CLASSY APPLES!"
-    end
+#     def apple()
+#         puts "I AM CLASSY APPLES!"
+#     end
 
-end
+# end
 
 
-# thing = MyStuff.new()
-# thing.apple()
-# puts thing.tangerine
+#  thing = MyStuff.new()
+#  thing.apple()
+#  puts thing.tangerine
 
 
 #----------------
 
-# class Song
+ class Song
 
-#   def initialize(lyrics)
-#     @lyrics = lyrics
-#   end
+   def initialize(lyrics)
+     @lyrics = lyrics
+   end
 
-#   def sing_me_a_song()
-#     @lyrics.each {|line| puts line }
-#   end
-# end
+  def sing_me_a_song()
+    @lyrics.each {|line| puts line }
+  end
+  
+ end
 
-# happy_bday = Song.new(["Happy birthday to you",
-#           "I don't want to get sued",
-#           "So I'll stop right there"])
+happy_bday = Song.new(["Happy birthday to you",
+          "I don't want to get sued",
+          "So I'll stop right there"])
+happy_bday.sing_me_a_song
 
 # bulls_on_parade = Song.new(["They rally around tha family",
 #             "With pockets full of shells"])
 
-# happy_bday.sing_me_a_song()
+ 
 
 # bulls_on_parade.sing_me_a_song()
 
--- module.rb
+++ module.rb
@@ -22,3 +22,35 @@
 
 
 
+# fred = Module.new do
+#  def meth1
+#     "hello"
+#  end
+#  def meth2
+#     "bye"
+#  end
+# end
+# a = "my string"
+# p a.methods.count
+# p a.class
+# p fred.class
+# p a.extend(fred)   #=> "my string"
+# p a.methods.count
+# p a.meth1          #=> "hello"
+# p a.meth2          #=> "bye"
+
+
+p Math.constants
+p Math.instance_methods   
+
+p Math::PI
+p Math::E
+
+
+# for module specification between two modules Scope resolution operator Module1::Module2
+# for one module and on class we seperate using Module.Class
+
+
+
+
+
