9e0e727602d64bacb65d6835883022da5e3ab194
saich <chsai.btech@gmail.com>
2016-04-30 16:57:59 +0000

    class and module examples are added

---
+++ module.rb
@@ -0,0 +1,24 @@
+#http://ruby-doc.org/core-2.3.0/Module.html
+
+#Modules Are Like Hashes
+
+#mystuff = {'apple' => "I AM APPLES!"}
+#puts mystuff['apple']
+
+# module MyStuff
+#     def MyStuff.apple()
+#         puts "I AM APPLES!"
+#     end
+
+   # this is just a variable
+   #TANGERINE = "Living reflection of a dream"
+    
+# end
+
+
+#mystuff['apple'] # get apple from dict
+#MyStuff.apple() # get apple from the module
+#MyStuff::TANGERINE # same thing, it's just a variable
+
+
+
---
+++ test.rb
@@ -0,0 +1,16 @@
+#ruby modules are like hashes
+
+module MathTest
+    def self.pi1()
+        return 3.14141414143435436346
+    end
+    
+    PI = 4.134343234
+  
+end
+p MathTest.pi
+p MathTest::PI
+
+# mathtest = {pi: 3.1414346464}
+# p mathtest[:pi]
+
